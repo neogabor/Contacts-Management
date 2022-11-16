488bce6c6b1697a15741fd891eb556ea38643e38
Gabor <mgfinal@gmail.com>
2022-11-16 11:27:58 +0100

    Finished api calls with middleware and db connection

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require('express')
+const connectDB = require('./config/db')
 
 const app = express()
+
+//Connect database
+connectDB()
+
+//Init Middleware
+app.use(express.json({ extended: false}))
 
 app.get('/', (req, res) => res.json({message: 'Welcome to the contact Keeper API.'}))
 
